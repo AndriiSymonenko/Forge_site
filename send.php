a9f2d24837798537c6e988caba812244be6bd942
Andrii Symonenko <andrii.symonenko.dev@gmail.com>
2020-07-17 09:42:47 +0300

    add about & catalog page fix bug

---
+++ send.php
@@ -0,0 +1,19 @@
+<?php
+
+	$name = $_POST['name'];
+	$phone = $_POST['phone'];
+	$email = $_POST['email'];
+
+	$sub = "Сообщение с сайта Кузня на конной";
+
+	$address = "iuriikuchma@gmail.com";//адрес куда будет отсылаться сообщение для администратора
+	$mes  = "Имя: $name \n";
+	$mes .= "E-mail: $email \n";	//в этих строчках мы заполняем текст сообщения. С помощью оператора .= мы просто дополняем текст в переменную
+	$mes .= "Телефон: $phone \n";
+	
+	$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");//собственно сам вызов функции отправки сообщения на сервере
+// 		 
+header('Refresh: 1; URL='.$_SERVER['HTTP_REFERER']);
+exit()
+
+?>
